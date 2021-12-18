4ae1803012fec36fec251af162555e23ff4ad8b5
Michael Krieger <phyre@phyre.me>
2021-12-18 13:46:25 -0500

    Allow a configurable log level
    Note: I could have used LOGLEVEL="${LOGLEVEL:-info}", but in case the shell isn't (always) bash, this is more compatible.

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+if [[ -z "${LOGLEVEL}" ]]; then
+  LOGLEVEL="info"
+else
+  LOGLEVEL="${LOGLEVEL}"
+fi
 umask 002
 sleep 15 && \
-deluge-web -L info --config /config
-deluged -d -L info --config /config
+deluge-web -L $LOGLEVEL --config /config
+deluged -d -L $LOGLEVEL --config /config
